77574b66623c2322a49d0c49fcaf3a2f8d33f6c0
Dako Mumladze <dmumladze@corp.europe-bet.com>
2018-12-20 15:09:05 +0400

    responsive layout changes2

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let logo = document.querySelector('#logo');
 let navbar = document.querySelector('#navigation');
 btn.addEventListener("click",function(){
     if(window.innerWidth < 768){
-        btn.classList.toggle('d-block');
+        btn.style.display="block";
         logo.classList.toggle('d-block');
         navbar.classList.toggle('d-block');
     }
