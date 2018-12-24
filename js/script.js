91b09dd9808211fb233fcba9255169385c4d5a9d
Dako Mumladze <dmumladze@corp.europe-bet.com>
2018-12-24 16:41:35 +0400

    animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 let btn = document.querySelector('#btnWrap');
-let logo = document.querySelector('#logo');
 let navbar = document.querySelector('#navigation');
 let navList = document.querySelector('.border');
 btn.addEventListener("click",function(){
     if(window.innerWidth < 768){
-        logo.classList.toggle('d-block');
         navbar.classList.toggle('d-block');
         navList.classList.toggle("d-mod");
     }
 });
-if(window.innerWidth < 768){
-   
-}
